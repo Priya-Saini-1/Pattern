337acf48bdd6cbded85a568c8881b54e1f9a3687
Priya <starlet202317@gmail.com>
2025-05-05 20:54:18 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, s;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for ( i = 1; i <= n; i++)
+    {
+       for( s = 1; s <= i - 1; s++)
+       {
+        printf("  ");
+       }
+
+       for( j = 1; j  <= n; j++) 
+       {
+        printf("* ");
+       }
+
+       printf("\n");
+
+    }
+    
+    return 0;
+
+}
